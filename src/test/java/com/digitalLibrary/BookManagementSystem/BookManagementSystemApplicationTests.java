e714f7ee1d8f3e9c3da72d340cf5ba25f36f4771
adithya-bucket <adhithya5518@gmail.com>
2025-03-26 09:55:57 -0500

    Initial Commit
    Created springboot project and publishing in GITHUB

---
+++ src/test/java/com/digitalLibrary/BookManagementSystem/BookManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalLibrary.BookManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
